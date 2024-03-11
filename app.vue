f50595695bd87ee0fef9f8995ac43391ed705143
Bruno Carsten de Araújo <brunocarsten@gmail.com>
2024-03-11 17:47:21 -0300

    header and intro content

---
+++ app.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+  import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+    <div class="lg:flex lg:justify-between lg:gap-4">
+      <Header />
+      <!-- <NuxtWelcome /> -->
+      <main id="content" class="pt-24 lg:w-1/2 lg:py-24">
+        <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Sobre mim">
+          <div
+            class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+          >
+            <h2 class="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
+          </div>
+          <div>
+            <p class="mb-4">
+              Sempre fui um entusiasta de tecnologia. Quando criança, gostava de desmontar meus video games e montá-los
+              novamente, só para ver o que havia dentro. A partir disso, por volta de 2010, comecei a demonstrar
+              interesse por programação. No primeiro ano do ensino médio, iniciei o curso técnico em informática, onde
+              fui introduzido aos conceitos básicos da computação: lógica de programação, algoritmos, arquitetura de
+              computadores, sistemas operacionais, redes, entre outros...
+            </p>
+            <p class="mb-4">
+              Atualmente, meu objetivo é desenvolver interfaces acessíveis e intuitivas para os clientes da
+              <NuxtLink
+                class="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
+                to="https://guiafacil.com"
+                target="_blank"
+              >
+                Guia Fácil Comunicação.
+              </NuxtLink>
+              Tenho o interesse em manter-me atualizado com as novas tecnologias e tendências do mercado, estabelecendo
+              um paralelo entre a evolução tecnológica e as reais necessidades do negócio. Busco identificar quais
+              tecnologias atenderão de forma mais eficiente à demanda, considerando sempre a eficácia em atender às
+              necessidades específicas da empresa.
+            </p>
+            <p>
+              Quando não estou diante de um computador, geralmente estou com minha esposa, minha família ou amigos. Ou
+              então, em algumas horas de lazer, me aventuro caçando algumas feras em Yharnam.
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  </div>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+  const navlinks = ref<any>([])
+
+  const options = useState(() => [
+    { text: 'Sobre', href: '#about' },
+    { text: 'Experiência', href: '#experience' },
+    { text: 'Projetos', href: '#projects' }
+  ])
+
+  const handleClick = (el: number) => {
+    navlinks.value.forEach((link: any) => {
+      link.$el.classList.remove('active')
+    })
+    const clickedElement = navlinks.value[el].$el
+    clickedElement.classList.toggle('active')
+  }
+</script>
+
+<template>
+  <header class="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+    <div>
+      <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Bruno Carsten de Araújo</h1>
+      <h2 class="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Senior Frontend Developer</h2>
+      <p class="mt-4 max-w-xs leading-normal">TEXTO INTRODUTORIO</p>
+      <nav class="nav hidden lg:block" aria-label="In-page jump links">
+        <ul class="mt-16 w-max">
+          <li v-for="({ text, href }, i) in options" :key="i">
+            <NuxtLink
+              @click="handleClick(i)"
+              :ref="(el) => (navlinks[i] = el)"
+              :class="`z-99 group flex items-center py-3 ${i === 0 ? 'active' : ''} `"
+              :href="href"
+            >
+              <span
+                class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+              >
+              </span>
+              <span
+                class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+              >
+                {{ text }}
+              </span>
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
