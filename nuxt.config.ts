444f4dfb4c6d616d48eee0c8d439366edd97eb0d
Bruno Carsten de Araújo <brunocarsten@gmail.com>
2024-05-16 17:16:04 -0300

    GTM implementation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   sourcemap: false,
   devtools: { enabled: true },
 
-  modules: ['nuxt-og-image', '@nuxt/fonts', '@nuxt/fonts'],
+  modules: ['nuxt-og-image', '@nuxt/fonts', '@nuxt/fonts', '@zadigetvoltaire/nuxt-gtm'],
 
   css: ['~/assets/css/main.css'],
 
@@ -47,32 +47,19 @@ export default defineNuxtConfig({
           name: 'description',
           content: 'Bruno é um desenvolvedor de software que cria produtos acessiveis e experiências digitais na web.'
         }
-      ],
-      link: [
-        {
-          rel: 'prefetch',
-          as: 'script',
-          href: `https://www.googletagmanager.com/gtag/js?id=${process.env.NUXT_TAG_MANAGER_ID}`
-        }
-      ],
-      script: [
-        {
-          type: 'text/javascript',
-          defer: true,
-          src: `https://www.googletagmanager.com/gtag/js?id=${process.env.NUXT_TAG_MANAGER_ID}`
-        },
-        {
-          type: 'text/javascript',
-          defer: true,
-          src: '/gtag.js'
-        }
       ]
     }
   },
 
   runtimeConfig: {
     public: {
-      TAG_MANAGER_ID: process.env.NUXT_TAG_MANAGER_ID
+      gtm: {
+        id: process.env.NUXT_TAG_MANAGER_ID,
+        defer: false,
+        loadScript: true,
+        enabled: true,
+        debug: true
+      }
     }
   },
 
