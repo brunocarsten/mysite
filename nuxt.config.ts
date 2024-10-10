bee52780567a8394cb1e6159ab4aa294913ed742
Bruno Carsten de Araújo <brunocarsten@gmail.com>
2024-10-10 16:05:20 -0300

    chore: nuxt-scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,20 @@
 export default defineNuxtConfig({
   sourcemap: false,
   devtools: { enabled: true },
-  modules: ['nuxt-og-image', '@nuxt/fonts', '@nuxt/fonts', '@zadigetvoltaire/nuxt-gtm'],
+  modules: ['nuxt-og-image', '@nuxt/fonts', '@nuxt/fonts', '@nuxt/scripts'],
   css: ['~/assets/css/main.css'],
 
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {}
-    }
+      autoprefixer: {},
+    },
   },
 
   app: {
     head: {
       htmlAttrs: {
-        lang: 'pt-br'
+        lang: 'pt-br',
       },
       charset: 'utf-8',
       title: 'Bruno Carsten de Araújo',
@@ -23,61 +23,61 @@ export default defineNuxtConfig({
         {
           hid: 'og:locale',
           name: 'og:locale',
-          content: 'pt_br'
+          content: 'pt_br',
         },
         {
           hid: 'og:site_name',
           name: 'og:site_name',
-          content: 'Bruno Carsten de Araújo'
+          content: 'Bruno Carsten de Araújo',
         },
         {
           hid: 'og:type',
           name: 'og:type',
-          content: 'website'
+          content: 'website',
         },
         {
           hid: 'og:url',
           name: 'og:url',
-          content: 'https://brunocarstenaraujo.com.br'
+          content: 'https://brunocarstenaraujo.com.br',
         },
         {
           hid: 'description',
           name: 'description',
-          content: 'Bruno é um desenvolvedor de software que cria produtos acessiveis e experiências digitais na web.'
-        }
-      ]
-    }
+          content: 'Bruno é um desenvolvedor de software que cria produtos acessiveis e experiências digitais na web.',
+        },
+      ],
+    },
   },
 
-  runtimeConfig: {
-    public: {
-      gtm: {
-        id: process.env.NUXT_TAG_MANAGER_ID,
-        defer: false,
-        loadScript: true,
-        enabled: true,
-        debug: true
-      }
-    }
+  $production: {
+    scripts: {
+      registry: {
+        googleAnalytics: process.env.NUXT_TAG_MANAGER_ID
+          ? {
+              id: process.env.NUXT_TAG_MANAGER_ID,
+            }
+          : undefined,
+      },
+    },
   },
 
   site: {
-    url: 'https://brunocarstenaraujo.com.br/'
+    url: 'https://brunocarstenaraujo.com.br/',
   },
 
   routeRules: {
-    '/**': { prerender: true }
+    '/**': { prerender: true },
   },
 
   experimental: {
     componentIslands: {
-      selectiveClient: true
-    }
+      selectiveClient: true,
+    },
   },
 
   nitro: {
-    static: true
+    static: true,
   },
 
-  compatibilityDate: '2024-08-16'
+  compatibilityDate: '2024-08-16',
 })
